2314b81d2694bd3379882f5fb68cf45beaa5042e
Rv072 <94162846+Rv072@users.noreply.github.com>
2022-01-19 19:47:40 +0530

    Add files via upload
    Basic Arithmetic Operators..In js

---
+++ Arthimati oprater/Arth-operator.js
@@ -0,0 +1,33 @@
+// Example-1. Addition operator
+
+var a = 20 , b = 30;
+c = a+b;
+console.log(c);
+
+// In java Script (+)operator also work as a concatination
+// Example-1.1
+
+var fname = "Ravi" , lname = "Kanthali";
+Full_name = (fname+" "+lname);
+console.log(Full_name);
+
+// Example.2 Subtraction
+var a = 33, b = 30;
+c = a-b;
+console.log(c);
+
+// Example.3 Multiplication
+var p = 12, q= 2;
+r = p * q;
+console.log(r);
+
+
+// Example.4 Division
+var x = 210 , y = 5;
+z = x / y;
+console.log(z); 
+// Example.5 Modulus (%)
+// Modulus operator only gives REMAINDER
+var k = 102 , l = 5;
+m = k % l;
+console.log(m);
